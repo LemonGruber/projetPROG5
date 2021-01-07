a22109d97c2aaf4a6749e3581efc9fd10ed4b978
vin212 <vincet-buniazet-07@hotmail.fr>
2021-01-07 10:31:54 +0100

    assemblage version Damien et Vincent

--- arm_branch_other.h
+++ arm_branch_other.h
@@ -1,32 +1,70 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #ifndef __ARM_BRANCH_OTHER_H__
 #define __ARM_BRANCH_OTHER_H__
+
+/**
+ * @file arm_branch_other.h
+ * @author G.Huard, Vincent, Yann
+ * @date 06/01/2021
+ * @brief gere les fonctions de branchement, les miscellaneous, et les coprocessor other 
+ * @todo comprendre ce qu'en les miscellaneous arm_branch_other.h ligne 31
+ */
 #include <stdint.h>
 #include "arm_core.h"
 
+/**
+ * @param p les information machine sous forme de structure de arm_core
+ * @param ins l'instruction en type uint32_t
+ * @return 0 si tout va bien 1 si non
+ * @brief gere toute les fonctions de branchement "simple"
+ */
 int arm_branch(arm_core p, uint32_t ins);
+
+/**
+ * @param p les information machine sous forme de structure de arm_core
+ * @param ins l'instruction en type uint32_t
+ * @return 0 si tout va bien 1 si non
+ * @brief gere toutes les fonction au niveau coprocesseur sauf, celle de load et store
+ */
 int arm_coprocessor_others_swi(arm_core p, uint32_t ins);
+
+/**
+ * @param p les information machine sous forme de structure de arm_core
+ * @param ins l'instruction en type uint32_t
+ * @return 0 si tout va bien 1 si non de type int
+ * @brief gere les fonctions miscellaneous
+ */
 int arm_miscellaneous(arm_core p, uint32_t ins);
+
+
+/**
+ * @param p les information machine sous forme de structure de arm_core
+ * @param cond la contion a tester avec son opcode de type uint8_t
+ * @param retour booleen de type char (0 si la condition est fausse 1 si non) de type pointeur de char
+ * @return 0 si tout vas bien 1 si non de type int
+ * @brief verifier si la condition est respecter ou non
+ */
+int cond_fonct (arm_core p, uint8_t cond, char *retour);
 
 #endif
--- arm_load_store.c
+++ arm_load_store.c
@@ -1,17 +1,20 @@
 /*
-armator - simulateur de jeu d'instruction armv5T � but p�dagogique
+Armator - simulateur de jeu d'instruction ARMv5T � but p�dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-teRmes de la Licence Publique G�n�rale GNU publi�e par la Free Software
+termes de la Licence Publique G�n�rale GNU publi�e par la Free Software
 Foundation (version 2 ou bien toute autre version ult�rieure choisie par vous).
+
 Ce programme est distribu� car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
 commercialisation ou d'adaptation dans un but sp�cifique. Reportez-vous � la
 Licence Publique G�n�rale GNU pour plus de d�tails.
+
 Vous devez avoir re�u une copie de la Licence Publique G�n�rale GNU en m�me
 temps que ce programme ; si ce n'est pas le cas, �crivez � la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
 �tats-Unis.
+
 Contact: Guillaume.Huard@imag.fr
 	 B�timent IMAG
 	 700 avenue centrale, domaine universitaire
@@ -23,110 +26,156 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 #include "debug.h"
 #include "registers.h"
-#include <stdio.h>
 
 int arm_load_store(arm_core p, uint32_t ins) {
+    int B, H, I, L, P, S, U, W;
+    int action, index, adresse;
+    int Rm, Rd, Rn;    
     
-    printf("\ndebut\n%u\n",ins);
+    int shift_imm;
+    int shift;
     
-    int Rm, Rd, Rn;
-    int L, S, H, I, U, W, P;
-    int action;
-    int shift, shift_imm;
-    
-    int valeur;
-    
-    uint32_t address;
-    uint32_t index;
     uint32_t value_word;
     uint16_t value_half;
     uint8_t value_byte;
     uint8_t offset_8;
    
-   offset_8 = 1;
-   offset_8 = offset_8;
-
-   
-    action = ins >> 25 & 0b111;
-    P = ins >> 24 & 1;
-    W = ins >> 21 & 1;
-    L = (ins >> 20) & 1;
-    U = (ins >> 23) & 1;
-    
-    Rn = (ins >> 16) & 0b1111;
     Rd = (ins >> 12) & 0b1111;
-    
-    if (action == 0)
+    Rn = (ins >> 16) & 0b1111;
+    W = (ins >> 21) & 1;
+    U = (ins >> 23) & 1;
+    P = (ins >> 24) & 1;
+    action = ins >> 25 & 0b111;
+    if (action == 0) // demi et double mots
     {
         S = (ins >> 6) & 1;
         H = (ins >> 5) & 1;
+        L = (ins >> 20) & 1;
         I = (ins >> 22) & 1;
-        
+
         if (I == 1)
         {
             offset_8 = ((ins >> 8) << 4) | (ins & 0b1111);
         }
         else
         {
-            //offset_8 prend la valeur de Rm
-            offset_8 = ins & 0xF;
+            offset_8 = ins & 0b1111; // offset au lie de Rm
         }
-            //Si non :
-            //On recupere la valeur dans le registre
-        valeur = arm_read_register(p,(ins >> 12) & 0b1111);
         
+        if (P == 0)
+        {
+            if (W == 1)
+            {
+                return UNDEFINED_INSTRUCTION; //UNPREDICTABLE;
+            }
+        }
+        
+        if (U == 1)
+        {
+            adresse = arm_read_register(p, Rn) + offset_8;
+        }
+        else
+        {
+            adresse = arm_read_register(p, Rn) - offset_8;
+        }
         
         if (L == 0 && S == 0 && H == 1)
         {
             //store halfword
-            arm_write_half(p,Rn,valeur);
+            if (P == 1 && W == 1)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
+            value_half = arm_read_register(p, Rd);
+            arm_write_half(p, arm_read_register(p, Rn), value_half);
+            if (P ==0 && W == 0)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
         }
         else if(L == 0 && S == 1 && H == 0 )
         {
             //load double
+            // Non pris en compte. #TODO
         }
         else if (L == 0 && S == 1 && H == 1)
         {
             //store double
+            // Non pris en compte. #TODO
         }
         else if (L == 1 && S == 0 && H == 1)
         {
             //load halfword unsigned
-            arm_read_half(p,Rn,&value_half);
-            
+            if (P == 1 && W == 1)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
+            arm_read_half(p,arm_read_register(p, Rn), &value_half);
+            arm_write_register(p, Rd, value_half);
+            if (P ==0 && W == 0)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
         }
         else if (L == 1 && S == 1 && H == 0)
         {
             //load byte signed
-            arm_read_byte(p,Rn,&value_byte);
+            if (P == 1 && W == 1)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
+            arm_read_byte(p,arm_read_register(p, Rn), &value_byte);
+            arm_write_register(p, Rd, value_byte);
+            if (P ==0 && W == 0)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
         }
         else if (L == 1 && S == 1 && H == 1)
         {
             //load halfword signed
-            arm_read_half(p,Rn,&value_half);
+            if (P == 1 && W == 1)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
+            arm_read_half(p,arm_read_register(p, Rn), &value_half);
+            arm_write_register(p, Rd, value_byte);
+            if (P ==0 && W == 0)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
         }
         else
         {
             //couille
         }
-        //Traitement pour un demi mot
        
+        if (P == 1)
+        {
+            if (W == 1)
+            {
+                arm_write_register(p, Rn, adresse);
+            }
+        }
+
     }
-    else // action == 1
+    else
     {
-        
+        L = (ins >> 20) & 1;
+        B = (ins >> 22) & 1;
         I = (ins >> 25) & 1;
-        
+
         if (I == 1)
         {
             Rm = ins & 0b1111;
-            shift_imm = (ins >> 7 ) & 0b11111;
+            shift_imm = (ins >> 7 ) &0b11111;
             shift = (ins >> 5) & 0b11;
-            
+            //Alors:
+            //Traitement pour un mot
             if (shift == 0b00)
             {
                 //LSL (declage a gauche)
-                index = arm_read_register(p,Rm) << shift_imm;
+                index = arm_read_register(p, Rm) << shift_imm;
             }
             else if (shift == 0b01)
             {
@@ -137,43 +186,40 @@ int arm_load_store(arm_core p, uint32_t ins) {
                 }
                 else
                 {
-                    index = arm_read_register(p,Rm) >> shift_imm;
+                    index = arm_read_register(p, Rm) >> shift_imm;
                 }
-            }   
+            }
             else if (shift == 0b10)
             {
                 //ASR (decalage arithmetique a droite)
-                
                 if (shift_imm == 0)
                 {
-                    int bit_sign = arm_read_register(p,Rm) >> 31 & 0b1;
-                    if (bit_sign)
+                    if (((Rm >> 31) & 1) == 1)
                     {
                         index = 0xFFFFFFFF;
                     }
-                    else 
+                    else
                     {
                         index = 0;
                     }
                 }
                 else
                 {
-                    index = arm_read_register(p,Rm) >> shift_imm;
+                    index = arm_read_register(p, Rm) >> shift_imm;
                 }
+                
             }
             else if (shift == 0b11)
             {
                 //ROR ou RRX
-                uint32_t C_flag = arm_read_register(p,CPSR);
-                C_flag = (C_flag >> 29) & 0b1;
                 if (shift_imm == 0)
                 {
-                    index = (C_flag << 31) | (arm_read_register(p,Rm) >> 1);
+                    index = ((arm_read_register(p, CPSR) >> 29) &0b1) || Rm >> 1;
                 }
-                else 
+                else
                 {
-                    index = arm_read_register(p,Rm) >> (shift_imm) & (arm_read_register(p,Rm) << (32-shift_imm));
-                }
+                    index = (arm_read_register(p, Rm) >> shift_imm) | (arm_read_register(p, Rm) << (32-shift_imm));
+                }                
             }
         }
         else // I == 0
@@ -182,70 +228,100 @@ int arm_load_store(arm_core p, uint32_t ins) {
             //On recupere le offset dans index
             index = ins & 0xFFF;
         }
+
         if (U == 1)
         {
-            address = arm_read_register(p,Rn) + index;
+            adresse = arm_read_register(p, Rn) + index;
         }
         else
         {
-            address = arm_read_register(p,Rn) + index;
+            adresse = arm_read_register(p, Rn) - index;
         }
+        
         if (P == 0)
         {  
             if (W == 0)
             {
                 if (L == 1)
                 {
-                    //Load word
-                    arm_read_word(p, Rn, &value_word);
-                    arm_write_register(p, Rd, value_word);
+                    if (B == 1) // Load unsigned byte
+                    {
+                        arm_read_byte(p, arm_read_register(p, Rn), &value_byte);
+                        arm_write_register(p, Rd, value_byte);
+                    }
+                    else       //Load word
+                    {
+                        arm_read_word(p, arm_read_register(p, Rn), &value_word);
+                        arm_write_register(p, Rd, value_word);
+                    }
                 }
                 else
                 {
-                    //Store word
-                    value_word = arm_read_register(p, Rd);
-                    arm_write_word(p, Rn, value_word);
+                    if (B == 1) // Store unsigned byte
+                    {
+                        value_byte = arm_read_register(p, Rd);
+                        arm_write_byte(p, arm_read_register(p, Rn), value_byte);
+                    }
+                    else        // Store word
+                    {
+                        value_word = arm_read_register(p, Rd);
+                        arm_write_word(p, arm_read_register(p, Rn), value_word);
+                    }
                 }
             }
             else // W == 1
             {
                 if (L == 1)
                 {
-                    //Load word
-                    arm_read_word(p, Rn, &value_word);
-                    arm_write_usr_register(p, Rd, value_word);
+                    if (B == 1) //Load unsigned byte
+                    {
+                        arm_read_byte(p, arm_read_register(p, Rn), &value_byte);
+                        arm_write_usr_register(p, Rd, value_byte);
+                    }
+                    else
+                    {
+                        //Load word
+                        arm_read_word(p, arm_read_register(p, Rn), &value_word);
+                        arm_write_usr_register(p, Rd, value_word);
+                    }
                 }
                 else
                 {
-                    //Store word
-                    value_word = arm_read_usr_register(p, Rd);
-                    arm_write_word(p, Rn, value_word);
+                    if (B == 1) // Store unsigned byte
+                    {
+                        value_byte = arm_read_usr_register(p, Rd);
+                        arm_write_byte(p, arm_read_register(p, Rn), value_byte);
+                    }
+                    else        //Store word
+                    {
+                        value_word = arm_read_usr_register(p, Rd);
+                        arm_write_word(p, Rn, value_word);
+                    }
                 }
             }
-            arm_write_register(p, Rn, address);
+            arm_write_register(p, Rn, adresse);
         }
         else // P == 1
         {
             if (W == 1)
             {
-                arm_write_register(p, Rn, address);
+                arm_write_register(p, Rn, adresse);
             }
             if (L == 1)
             {
                 //Load word
-                arm_read_word(p, Rn, &value_word);
-                arm_write_usr_register(p, Rd, value_word);
+                arm_read_word(p, adresse, &value_word);
+                arm_write_register(p, Rd, value_word);
             }
             else
             {
                 //Store word
-                value_word = arm_read_usr_register(p, Rd);
-                arm_write_word(p, Rn, value_word);
+                value_word = arm_read_register(p, Rd);
+                arm_write_word(p, adresse, value_word);
             }
         }
-        printf("\nfin\n");
     }
-    arm_afficher_mem (p);
+    
     return 0;
 }
 
